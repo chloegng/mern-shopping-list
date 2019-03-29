48a2bc7f6f158366217a65e41d2cd1ae37dd7bdf
Chloe <chloegng@gmail.com>
2019-03-28 22:44:59 -0700

    add item action

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,11 @@ import './App.css';
 import { Provider } from 'react-redux';
 import store from './store';
 
-import AppNavbar from './components/AppNavbar'
-import ShoppingList from './components/ShoppingList'
+import { Container } from 'reactstrap'
+
+import AppNavbar from './components/AppNavbar';
+import ItemModal from './components/ItemModal';
+import ShoppingList from './components/ShoppingList';
 
 
 class App extends Component {
@@ -15,7 +18,10 @@ class App extends Component {
       <Provider store={store}>
         <div className="App">
           <AppNavbar />
-          <ShoppingList />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
         </div>
       </Provider>
     );
