3538c64340c25e7fe400d50ef18441e9d731ecbb
Chloe <chloegng@gmail.com>
2019-04-04 21:46:10 -0700

    deploy to heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-//const mongodb = require('mongodb');
 const config = require('config')
+const path = require('path');
 
 const app = express();
 
@@ -25,7 +25,15 @@ mongoose
   app.use('/api/users', require('./routes/api/users'));
   app.use('/api/auth', require('./routes/api/auth'));
 
+  // Serve static assets if in production
+  if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
 
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+  }
 
   const port = process.env.PORT || 5000;
 
